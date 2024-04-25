2bbd7359896a52ecf9d87187391236dac0080bcb
Ahimsa Das <ahimsadas@Ahimsas-MacBook-Pro.local>
2024-04-26 03:26:58 +0530

    First commit

---
+++ run.command
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+source script/venv/bin/activate  # Activate virtual environment if needed
+python3 script/main.py
---
+++ script/main.py
@@ -0,0 +1,49 @@
+import os
+import requests
+import csv
+
+# Function to fetch data from the API
+def fetch_data(query, page):
+    url = f"https://6rhg85xpv5.execute-api.us-east-1.amazonaws.com/Prod/originators/?q={query}&page={page}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Failed to fetch data for query {query} and page {page}")
+        return None
+
+# Main function to iterate over queries and pages and save data to CSV
+def main():
+    # Create 'output' directory if not present
+    if not os.path.exists('output'):
+        os.makedirs('output')
+
+    with open("output/all_data.csv", "w", newline="", encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        total_data_count = 0
+        total_queries = 26 ** 3
+        query_count = 0
+        for i in range(97, 123):  # ASCII codes for lowercase letters a-z
+            for j in range(97, 123):
+                for k in range(97, 123):
+                    query = chr(i) + chr(j) + chr(k)  # Generating query strings
+                    page = 1
+                    while True:
+                        data = fetch_data(query, page)
+                        if data and 'data' in data and 'items' in data['data']:
+                            data_count = len(data['data']['items'])
+                            total_data_count += data_count
+                            for item in data['data']['items']:
+                                writer.writerow(item.values())
+                            if page == data['data']['last']:
+                                break
+                            else:
+                                page += 1
+                        else:
+                            break
+                    query_count += 1
+                    print(f"Progress: Query {query_count}/{total_queries} processed. Total data added: {total_data_count}", end='\r')
+    print("\nData retrieval and CSV creation completed. Total data added:", total_data_count)
+
+if __name__ == "__main__":
+    main()
